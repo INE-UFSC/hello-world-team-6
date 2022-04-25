6172901e01703dd54a98dc82bf75a78487b2ed76
Otávio <otaviomafras2@gmail.com>
2022-04-25 17:38:13 -0300

    add Apresentação Otávio

--- membro2.py
+++ membro2.py
@@ -1,2 +1,3 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Otávio, tenho 21 anos de idade e sou um aluno de Ciência da Computação na UFSC. Um dos meus hobbies favoritos é jogar Overwatch, mas outros também incluem jogar rpg de mesa, outros jogos, ouvir músicas. O que eu espero da matéria é aprimorar meus conhecimentos sobre programação." 
+	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
