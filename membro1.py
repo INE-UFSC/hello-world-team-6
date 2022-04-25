cb65f0ba7efaa6fa2002c89b5ffbc6985208187b
Matheus S <matheussouza4555@gmail.com>
2022-04-25 10:57:46 -0300

    Add apresentacao Matheus

--- membro1.py
+++ membro1.py
@@ -1,2 +1,3 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, meu nome é Matheus Antonio, tenho 21 anos, jogo RPG de mesa e jogos de tabuleiro, e já fiz alguns projetos bobos em Python como hobby. Espero entender POO mais a fundo para desenvolver sistemas sem ser apenas digitar códigos sem pensar muito.\n" 
+	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
